4d1d2d0100b645309cffb4416a03f539728cbc36
Project-Crackers <gopinath21911@gmail.com>
2025-11-20 11:27:52 +0530

    Initial Commit

---
+++ src/main/java/com/springBoot/flightBooking/entity/Payment.java
@@ -0,0 +1,41 @@
+package com.springBoot.flightBooking.entity;
+
+import java.time.LocalDate;
+
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.springBoot.flightBooking.dto.PaymentMode;
+import com.springBoot.flightBooking.dto.PaymentStatus;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = "booking")
+@EqualsAndHashCode(exclude = "booking")
+public class Payment {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@CreationTimestamp
+	private LocalDate paymentDate;
+	private Double amount;
+	
+	@Enumerated(EnumType.STRING)
+	private PaymentMode mode;
+	
+	@Enumerated(EnumType.STRING)
+	private PaymentStatus status;
+
+	@JsonIgnore
+	@OneToOne(mappedBy = "payment")
+	private Booking booking;
+
+}
